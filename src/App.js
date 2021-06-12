57093bb7cb2b0667de0543b97e9c600ef152d420
Sarah Gentry <sagentr5@gmail.com>
2021-06-12 17:54:27 -0500

    naviagtion bar works using routing

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import { Route, Switch } from "react-router-dom";
 import AllMeetupsPage from "./pages/AllMeetups";
 import NewMeetUpsPage from "./pages/NewMeetup";
 import FavoritesPage from "./pages/Favorites";
+import MainNavigation from './components/layout/MainNavigation'
 
 function App() {
   return (
     <div>
+      <MainNavigation />
       <Switch>
         <Route path="/" exact={true}>
           <AllMeetupsPage />
